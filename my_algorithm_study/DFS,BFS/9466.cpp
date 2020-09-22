1ff7612041102859c5f7e0213b50b9dbef4a2c58
imjoonghyeon <joongh0113@gmail.com>
2020-09-22 11:37:22 +0900

    first_version

--- my_algorithm_study/DFS,BFS/9466.cpp
+++ my_algorithm_study/DFS,BFS/9466.cpp
@@ -6,16 +6,37 @@ using namespace std;
 
 int main() {
 	int T; cin >> T;
-	int n, st, cur;
+	int n, st;
 	int cnt;
+	int cur, nxt;
 	while (T--) {
 		cin >> n;
 		cnt = 0;
-
-		vector<bool>visit(n+1, false);
 		vector<int>project(n + 1);
-
 		for (int i = 1; i <= n; ++i) cin >> project[i];
+		
+		for (int i = 1; i <= n; ++i) {
+			if(project[i]){
+				st = i;
+				queue<int>q;
+				q.push(i);
+				while (!q.empty()) {
+					cur = q.front();
+					nxt = project[cur];
+					q.pop();
+					if (st != nxt && cur != nxt) q.push(nxt);
+				}
+				if (st == nxt) {
+					cur = st;
+					while (project[cur]) {
+						nxt = project[cur];
+						project[cur] = 0;
+						cur = nxt;
+					}
+				}
+				else cnt++;
+			}
+		}
 		cout << cnt << '\n';
 	}
 
